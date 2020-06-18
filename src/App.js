654ab5eaf8e9e90ebd28df0a4566b4963f7185b3
Kristie Huang <kristiehuang01@gmail.com>
2020-06-17 19:35:51 -0700

    added a bit of logic, but it's running async

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+/** Find out how diverse a comma-separated list of names is - perhaps
+ * for a mailing list, a company employee list, etc. Just for fun! 
+ * Using the diversityAPI tool.
+ * How many women and people of color are on your teams?
+ * Author: Kristie Huang
+ */
+
 import React, { Component, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -9,38 +16,84 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+    //STATE VS CLASS VAR = SETSTATE AUTO CALLS RENDER
     this.state = {
-      names: ""
+      input: "",
+      names: [],
     };
 
   }
+  womenTotal = 0;
+  womenTotalConfidence = 0;
+  pocTotal = 0;
+  pocTotalConfidence = 0;
+
 
   handleSubmit = event => {
     event.preventDefault();
-    alert('An essay was submitted: ' + this.state.names);
-    axios
-      .get(`https://api.diversitydata.io/?fullname=${this.state.names}`)
-      .then((response) => {
-        const { data } = response;
-        this.calculatePercentage(data);
-        console.log(data);
-      })
-      .catch((error) => { this.calculatePercentage(false); });
+    alert('Checking this list of names: ' + this.state.input);
+    const names = this.state.input.split(", ");
+    console.log(names);
+
+    this.calculatePercentage(names);
+
+    this.setState({
+      input: this.state.input,
+      names: names
+    });
+
   }
 
   handleChange = event => {
     const { name, value } = event.target;
-    this.setState({ names:  value});
-    console.log(event.target.value);
-    console.log(this.state);
+    this.setState({
+      input: value,
+      names: this.state.names,
+    });
+
 
   }
 
-  calculatePercentage() {
+  /** The logic. */
+  calculatePercentage(names) {
+    if (!names) {
+      //error handling
+    }
+
+    for (const name of names) {
+      //calculate gender + likelihood
+      axios
+        .get(`https://api.diversitydata.io/?fullname=${name}`)
+        .then((response) => {
+          const { data } = response;
+          console.log("before" + this.womenTotal);
+          console.log(data["gender"] === "female");
+
+          this.womenTotal = data["gender"] === "female" ? this.womenTotal + 1 : this.womenTotal;
+          this.womenTotalConfidence += data["gender probability"];
+          this.pocTotal = data["ethnicity"] !== "white" ? this.pocTotal + 1 : this.pocTotal;
+          this.pocTotalConfidence += data["ethnicity probability"];
+          console.log(this.womenTotal);
+
+
+        })
+        .catch((error) => { this.calculatePercentage(false); });
+      
+    }
+
+
 
   }
 
   render() {
+
+
+    //i want to display - gender, ethnicities
+    const res = this.state.names.map((key, num) => {
+      return (
+        <p>{key}</p>
+      );
+    });
 
     return (
         <div className="App">
@@ -49,7 +102,7 @@ class App extends Component {
           <form onSubmit={this.handleSubmit}>
             <Input
               name="names"
-              value={this.state.names}
+              value={this.state.input}
               onChange={this.handleChange}
               label="Employees' names"
               helperText="(E.g. Abby Alligator, Ben Bulldog)"
@@ -58,7 +111,14 @@ class App extends Component {
               rowsMax={8}
             />
             <Button type="submit">Add</Button>
-          </form>
+        </form>
+        {console.log("women total!" + this.womenTotal)}
+        <p>Percentage women: {this.womenTotal / this.state.names.length}%</p>
+
+        <p>Percentage people of color: {this.pocTotal / this.state.names.length}%</p>
+        <h1>List of names:</h1>
+        {res}
+
 
         </div>
       );
