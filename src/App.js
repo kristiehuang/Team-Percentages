e8ef4544f360459b8a0a47d1907f64df51dfa911
Kristie Huang <kristiehuang01@gmail.com>
2020-06-17 19:48:47 -0700

    worksgit add .git add .git add .git add . still need to add confidence percentage

--- src/App.js
+++ src/App.js
@@ -20,13 +20,14 @@ class App extends Component {
     this.state = {
       input: "",
       names: [],
+      womenTotal: 0,
+      womenTotalConfidence: 0,
+      pocTotal: 0,
+      pocTotalConfidence: 0,
     };
 
   }
-  womenTotal = 0;
-  womenTotalConfidence = 0;
-  pocTotal = 0;
-  pocTotalConfidence = 0;
+
 
 
   handleSubmit = event => {
@@ -38,7 +39,6 @@ class App extends Component {
     this.calculatePercentage(names);
 
     this.setState({
-      input: this.state.input,
       names: names
     });
 
@@ -66,14 +66,17 @@ class App extends Component {
         .get(`https://api.diversitydata.io/?fullname=${name}`)
         .then((response) => {
           const { data } = response;
-          console.log("before" + this.womenTotal);
+          console.log("before" + this.state.womenTotal);
           console.log(data["gender"] === "female");
 
-          this.womenTotal = data["gender"] === "female" ? this.womenTotal + 1 : this.womenTotal;
-          this.womenTotalConfidence += data["gender probability"];
-          this.pocTotal = data["ethnicity"] !== "white" ? this.pocTotal + 1 : this.pocTotal;
-          this.pocTotalConfidence += data["ethnicity probability"];
-          console.log(this.womenTotal);
+          this.setState({
+            womenTotal: data["gender"] === "female" ? this.state.womenTotal + 1 : this.state.womenTotal,
+            womenTotalConfidence: this.state.womenTotalConfidence + data["gender probability"],
+            pocTotal: data["ethnicity"] !== "white" ? this.state.pocTotal + 1 : this.state.pocTotal,
+            pocTotalConfidence: this.state.pocTotalConfidence + data["ethnicity probability"],
+          });
+
+          console.log(this.state.womenTotal);
 
 
         })
@@ -89,15 +92,22 @@ class App extends Component {
 
 
     //i want to display - gender, ethnicities
-    const res = this.state.names.map((key, num) => {
+    const allNameLabels = this.state.names.map((key, num) => {
       return (
         <p>{key}</p>
       );
     });
 
+    const womenPercent = this.state.womenTotal ?
+      this.state.womenTotal * 100 / this.state.names.length : 0;
+    
+    const pocPercent = this.state.pocTotal ?
+      this.state.pocTotal * 100 / this.state.names.length : 0;
+
     return (
         <div className="App">
-          <p>How white is your team?</p>
+        <p>How many women and people of color are on your teams?</p>
+        <p>Comma-separated list, please.</p>
 
           <form onSubmit={this.handleSubmit}>
             <Input
@@ -112,12 +122,14 @@ class App extends Component {
             />
             <Button type="submit">Add</Button>
         </form>
-        {console.log("women total!" + this.womenTotal)}
-        <p>Percentage women: {this.womenTotal / this.state.names.length}%</p>
+        {console.log("women total!")}
 
-        <p>Percentage people of color: {this.pocTotal / this.state.names.length}%</p>
+        {console.log(this.state)}
+        <p>Percentage women: {womenPercent}%</p>
+
+        <p>Percentage people of color: {pocPercent}%</p>
         <h1>List of names:</h1>
-        {res}
+        {allNameLabels}
 
 
         </div>
