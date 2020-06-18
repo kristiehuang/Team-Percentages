6d6fa72dfa747a80e255716865487314bdf464ae
Kristie Huang <kristiehuang01@gmail.com>
2020-06-17 18:35:38 -0700

    Implement API call upon submit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      names: []
+      names: ""
     };
 
   }
@@ -18,18 +18,30 @@ class App extends Component {
   handleSubmit = event => {
     event.preventDefault();
     alert('An essay was submitted: ' + this.state.names);
-
+    axios
+      .get(`https://api.diversitydata.io/?fullname=${this.state.names}`)
+      .then((response) => {
+        const { data } = response;
+        this.calculatePercentage(data);
+        console.log(data);
+      })
+      .catch((error) => { this.calculatePercentage(false); });
   }
 
   handleChange = event => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ names:  value});
     console.log(event.target.value);
     console.log(this.state);
 
   }
 
+  calculatePercentage() {
+
+  }
+
   render() {
+
     return (
         <div className="App">
           <p>How white is your team?</p>
